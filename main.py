23e097e29081183df66aec78f5cf2f1f8d954cee
韩于惟 <hanyuwei70@qq.com>
2018-05-10 16:05:14 +0800

    sync

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 from config import TELEGRAM_BOT_TOKEN
 import os, logging, sys
 
-from telegram.ext import Updater
+from telegram.ext import Updater, CommandHandler
+import telegram
 
 TOKEN = None
 logger = logging.getLogger(__name__)
@@ -19,7 +20,20 @@ logger.addHandler(loghandler)
 
 
 def clocker(bot, update):
-    pass
+    """
+    处理定时提醒相关的命令
+    :param bot: bot handle
+    :param update: 更新
+    :return:
+    """
+    userid = update['from']['id']
+    chatid = update['message']['chat']['chatid']
+    username = update['from']['username']
+    text = update['text']
+    logger.info("got update: From:%s(%d) %s"%(username,userid,text))
+    if text == "/clocker":
+        bot.send_message(chat_id=chatid, text="提醒服务~", reply_markup=telegram.ReplyKeyboardMarkup(["查看"]))
+
 
 def loadconfig():
     global TOKEN
@@ -32,8 +46,11 @@ def loadconfig():
 def main():
     loadconfig()
     updaterhandle = Updater(token=TOKEN)
-    aboutme=updaterhandle.bot.get_me()
+    aboutme = updaterhandle.bot.get_me()
     logger.info("About me: ID:%d username:%s" % (aboutme['id'], aboutme['username']))
+    dispatcher = updaterhandle.dispatcher
+    dispatcher.add_handler(CommandHandler('clocker', clocker))
+    updaterhandle.start_polling()
     return
 
 
